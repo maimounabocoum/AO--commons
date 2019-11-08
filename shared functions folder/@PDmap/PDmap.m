44ba5034ab63bd1906f7d1f1cabfa78b745ed21a
Maimouna Bocoum <maimouna.bocoum@gmail.com>
2019-11-08 14:55:16 +0100

    addind structure for photodiode acquisition

---
+++ shared functions folder/@PDmap/PDmap.m
@@ -0,0 +1,19 @@
+classdef PDmap
+    %UNTITLED2 Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        
+        RAW
+        
+    end
+    
+    methods
+        
+        
+        
+        
+    end
+    
+end
+
