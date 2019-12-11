a36bac20e1c6eac7cd2f215d9376e07dae01e50b
Maimouna Bocoum <maimouna.bocoum@gmail.com>
2019-12-11 19:55:16 +0100

    ktu

---
+++ common Data Analysis/GetIntensity.m
@@ -0,0 +1,21 @@
+function [frame,P_tot] = GetIntensity( IM , BG , MyXimea )
+
+
+%% define of camera buffer used for analysis:
+MyXimea.frame = IM( 1200 + (1:2^10) , 1700 + (1:2^10) );
+MyXimea.frame_bg = BG( 1200 + (1:2^10) , 1700 + (1:2^10) );
+
+
+% remove BG
+MyXimea.frame = MyXimea.frame - MyXimea.frame_bg ;
+
+% MyXimea = MyXimea.ConvertFrame2electrons;
+ %MyXimea = MyXimea.ConvertFrame2photons;
+ MyXimea = MyXimea.ConvertFrame2Intensity; % in W/m^2
+
+% evaluate total incident power
+P_tot = sum( MyXimea.frame(:)*MyXimea.dpixel*MyXimea.dpixel );
+frame = MyXimea.frame;
+
+end
+
