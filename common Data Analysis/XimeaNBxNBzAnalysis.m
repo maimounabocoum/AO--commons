ced54b6c79a9a980e2820f7f1885aaf15b5b288b
maimouna bocoum <maimouna.bocoum@gmail.com>
2020-01-21 17:03:01 +0100

    apres tests avec JM

--- common Data Analysis/XimeaNBxNBzAnalysis.m
+++ common Data Analysis/XimeaNBxNBzAnalysis.m
@@ -54,15 +54,41 @@ myFilter = ImageFilter( [600 320 90 90] );
 BW = myFilter.getROI(N,N);
 myFilter.DrawROI;
 %% -------------- begin loop 
+ObjectFFT = zeros(2^5 , 2^5);
+ObjectFFT2= zeros(2^5 , 2^5);
+NBloop = unique(NB(:,1));
+
+for loop1 = 1:length(NBloop)
+    loop_scan = find( NB(:,1)==NBloop(loop1) ) ;
+    for loop = loop_scan'%1:Nfiles
+    IndexRecord = ExtractIndex(Filename{loop}) - IndexRecord0 ;
+    Nbx = NB(mod(IndexRecord,Nfiles)+1,2) ;
+    Nbz = NB(mod(IndexRecord,Nfiles)+1,3) ;
+  ObjectFFT2((2^4+1)+Nbz,(2^4+1)+Nbx) = mean(mean(Gag.raw(5000:6000,loop_scan),1),2);
+    end
+  
+end
+figure;surfc(ObjectFFT2(18:27,12:22))
+
+
+for loop1 = 1:length(NBloop)
+    
 IndexRecord0 = ExtractIndex(Filename{1});
 IM_rec  = zeros(N,N);
 
+
+    
+    loop_scan = find( NB(:,1)==NBloop(loop1) ) ;
+    
 ImageCorr = zeros(1,Nfiles);
 G = TF2D( 2^5 , 2^5 , (2^5-1)*nuX0 , (2^5-1)*nuZ0 );
-ObjectFFT = zeros(2^5 , 2^5);
 
 
-for loop = 1:Nfiles
+
+
+
+%% loop_scan'
+for loop = 37:40%1:Nfiles
     
 %     if loop == 1
 %     IM = double( importdata([Foldername,Filename{1}]) );
@@ -74,11 +100,15 @@ for loop = 1:Nfiles
     [Frame,P_frame] = GetIntensity( IM , BG , MyXimea );
     
     
+    
     % filter out tagged photons
     FrameFFT      = F.fourier( Frame );
     % figure;imagesc(log(abs(FrameFFT)));
     FilteredFrame = F.ifourier( FrameFFT.*BW) ;
-    FilteredFrame = 2*abs(FilteredFrame).^2;%./(Frame_ref);
+    
+    FourFrames(loop-36,:,:) = FilteredFrame;
+    
+    % FilteredFrame = 2*abs(FilteredFrame).^2;%./(Frame_ref);
     %FilteredFrame(abs(FilteredFrame) > 10 ) = 0 ;
     %FilteredFrame(800:end,:)=0;
     % evaluation of total power
@@ -93,43 +123,85 @@ for loop = 1:Nfiles
     Nbz = NB(mod(IndexRecord,Nfiles)+1,3) ;
     PHASE= NB(mod(IndexRecord,Nfiles)+1,4)+pi/2;
     
-    DecalZ  =   0.3;
-    s = exp(2i*pi*DecalZ*Nbz);
-    ObjectFFT((2^4+1)+Nbz,(2^4+1)+Nbx) = ObjectFFT((2^4+1)+Nbz,(2^4+1)+Nbx) + s*exp(1i*2*pi*PHASE)*P_tot(loop);
-    ObjectFFT((2^4+1)-Nbz,(2^4+1)-Nbx) = conj( ObjectFFT((2^4+1)+Nbz,(2^4+1)+Nbx));
+%     DecalZ  =   0.3;
+%     s = exp(2i*pi*DecalZ*Nbz);
+%     ObjectFFT((2^4+1)+Nbz,(2^4+1)+Nbx) = ObjectFFT((2^4+1)+Nbz,(2^4+1)+Nbx) + s*exp(1i*2*pi*PHASE)*P_tot(loop);
+%     ObjectFFT((2^4+1)-Nbz,(2^4+1)-Nbx) = conj( ObjectFFT((2^4+1)+Nbz,(2^4+1)+Nbx));
 
     % direct reconstruction
     IM_rec = IM_rec + exp(1i*2*pi*PHASE)*FilteredFrame;
 %     
-%     figure
+%    figure
 % imagesc( F.x*1e3 , F.z*1e3 , 100*FilteredFrame )
 % cb = colorbar ;
 % xlabel('mm')
 % ylabel('mm')
 % ylabel(cb,'Intensity in \mu W /cm^2')
 % title(['P_{tot}',num2str(1e6*P_tot(loop)),'\mu W , (NBx,NBz,phase)=(',num2str(Nbx),',',num2str(Nbz),',',num2str(PHASE),')'])
-% % ImageCorr(loop) = corr2(Frame,Frame0)    ;
-% % subplot(122)    
-% % FilteredFrame_fft = F.fourier(FilteredFrame);
-% % %FilteredFrame_fft(512:514,512:514)= 0;
-% % imagesc( F.fx*1e3 , F.fz*1e3 , UnwrapPHASE(angle(FilteredFrame_fft),512,512))
-% % colorbar
-% % axis([-1 1 -1 1]*0.5e7)
-% % caxis([-30 30])
-% drawnow
+% ImageCorr(loop) = corr2(Frame,Frame0)    ;
+% subplot(122)    
+% FilteredFrame_fft = F.fourier(FilteredFrame);
+% %FilteredFrame_fft(512:514,512:514)= 0;
+% imagesc( F.fx*1e3 , F.fz*1e3 , UnwrapPHASE(angle(FilteredFrame_fft),512,512))
+% colorbar
+% axis([-1 1 -1 1]*0.5e7)
+% caxis([-30 30])
+drawnow
 % saveas(gcf,['D:\Data\Mai\2020-01-13\4-phase\',Filename{loop}],'png');
 end
 
+%
 
-% figure(2)
-% subplot(131); imagesc(real(IM_rec(100:750,100:800))); colorbar ;
-% title(['Real : (NBx,NBz)=(',num2str(Nbx),',',num2str(Nbz),')'])
-% subplot(132); imagesc(imag(IM_rec(100:750,100:800))); colorbar ;
-% title(['Imag : (NBx,NBz)=(',num2str(Nbx),',',num2str(Nbz),')'])
-% subplot(133); imagesc(abs(IM_rec(100:750,100:800))); colorbar ;
-% caxis([0 7e-3])
+cal = 2.75;
+nuX = Nbx*nuX0;
+nuZ = Nbz*nuZ0;
+alpha = nuX/nuZ;
+
+calX = 6.6;
+calY = 7;
+
+[X,Z] = meshgrid(F.x,F.z);
+PHASE0 = exp(2*1i*pi*(calX*nuX*X +calY*nuZ*Z));
+PHASE0 = PHASE0(100:750,100:800);
+
+figure(2)
+subplot(121); imagesc(real(IM_rec(100:750,100:800))); colorbar ;
+title(['Real : (NBx,NBz)=(',num2str(Nbx),',',num2str(Nbz),')'])
+subplot(122); imagesc(imag(IM_rec(100:750,100:800))); colorbar ;
+title(['Imag : (NBx,NBz)=(',num2str(Nbx),',',num2str(Nbz),')'])
+% %subplot(223); imagesc(abs(IM_rec(100:750,100:800))); colorbar ;
+% subplot(223); imagesc(cal*F.x*1e3,cal*F.z*1e3,angle(PHASE0)); colorbar ;
+% %subplot(224); imagesc(cal*F.x*1e3,cal*F.z*1e3,angle(IM_rec(100:750,100:800).*PHASE0)); colorbar ;
+% subplot(224); imagesc(cal*F.x*1e3,cal*F.z*1e3,abs(IM_rec(100:750,100:800))); colorbar ;
+
+% r = IM_rec(100:750,100:800).*PHASE0;
+% r = r(200:500,200:500);
+
+% ObjectFFT((2^4+1)+Nbz,(2^4+1)+Nbx) = (mean(abs(r(:))));
+
+% mean(Gag.raw(5000:6000,:),2)
+
+%caxis([0 7e-3])
 % title(['Real : (NBx,NBz)=(',num2str(Nbx),',',num2str(Nbz),')'])
 % saveas(gcf,['D:\Data\Mai\2020-01-14\data analysis\Imag(NBx,NBz)(',num2str(Nbx),',',num2str(Nbz),')'],'png');
+
+end
+
+%%
+
+[Xf,Zf] = meshgrid(-5:5,1:10);
+
+PhasL =  1.1239*Xf + 0.718*Zf - 3.939;
+figure(6);
+subplot(121); surfc( -PhasL + UnwrapPHASE(ObjectFFT(18:27,12:22),5,5) );
+xlabel('NbX')
+ylabel('NbZ')
+view([-58.8340 17.8721])
+colorbar
+subplot(122); imagesc(PhasL);
+colorbar
+xlabel('NBx')
+ylabel('NbZ')
 
 %% interpolation of main object onto this grid:
 MAIN_FFT = F.fourier(MAIN);
@@ -140,13 +212,15 @@ MAIN_FFT(1:((F.Nz/2+1)-6),:) = 0;
 MAIN_FFT(:,(F.Nx/2+1)+6:end) = 0;
 MAIN_FFT(:,1:((F.Nx/2+1)-6)) = 0;
 MAIN_FFT(:,1:((F.Nx/2+1)-6)) = 0;
-MAIN_FFT([F.Nz/2,F.Nz/2+2],F.Nx/2+1) = 0.01*MAIN_FFT([F.Nz/2,F.Nz/2+2],F.Nx/2+1);
-figure(4);imagesc((1/cal)*F.fx/G.dfx,(1/cal)*F.fz/G.dfz,real(MAIN_FFT))
+%MAIN_FFT([F.Nz/2,F.Nz/2+2],F.Nx/2+1) = MAIN_FFT([F.Nz/2,F.Nz/2+2],F.Nx/2+1);
+figure(4);imagesc((1/cal)*F.fx/G.dfx,(1/cal)*F.fz/G.dfz,abs(MAIN_FFT))
 axis([-15 15 -10 10])
 xlabel('NbX')
 ylabel('NbZ')
 colorbar
+
 MAIN_filtered = F.ifourier(MAIN_FFT);
+MAIN_filtered = MAIN_filtered - ones(Nfft,1)*MAIN_filtered(1,:);
 figure(5);imagesc(cal*F.x*1e3,cal*F.z*1e3,MAIN_filtered)
 
 %% loop to get fourier componant:
@@ -155,15 +229,16 @@ Nfft = 2^10;
 G = TF2D( Nfft , Nfft , (Nfft-1)*nuX0 , (Nfft-1)*nuZ0 );
 ObjectFFT = zeros(Nfft , Nfft);
 
+Theta = -4.7124;
 for loop = 1:length(P_tot)
     Nbx     = NB(loop,2) ;
     Nbz     = NB(loop,3) ;
     PHASE   = NB(loop,4);
     
     DecalZ  =   -0.2; % ??
-    chirpZ  =   0.3;
+    chirpX  =   0*0.0018;
     DecalX  =   -0.2; % ??
-    s = exp(2i*pi*(DecalZ*Nbz + chirpZ*Nbz.^2 + DecalX*Nbx));
+    s = 1i*exp(2i*pi*(DecalZ*Nbz + chirpX*Nbx.^2 + DecalX*Nbx));
    
     ObjectFFT((Nfft/2+1)+Nbz,(Nfft/2+1)+Nbx) = ObjectFFT((Nfft/2+1)+Nbz,(Nfft/2+1)+Nbx) + s*exp(1i*2*pi*PHASE)*P_tot(loop);
     ObjectFFT((Nfft/2+1)-Nbz,(Nfft/2+1)-Nbx) = conj( ObjectFFT((Nfft/2+1)+Nbz,(Nfft/2+1)+Nbx) );   
@@ -182,7 +257,7 @@ end
 
 Reconstruct = G.ifourier( ObjectFFT );
 % % I = ifft2(ifftshift(ObjectFFT));
-% % I = I - ones(2^5,1)*I(1,:);
+Reconstruct = Reconstruct - ones(Nfft,1)*Reconstruct(1,:);
 % % I = ifftshift(I,2);
 figure(1);imagesc(G.x*1e3,G.z*1e3,real(Reconstruct))
 title('reconstructed object')
