ffabafff1c6f264d526007c24b981c516c14ae1b
maimouna bocoum <maimouna.bocoum@gmail.com>
2021-11-26 13:54:06 +0100

    adding FWHM retrieval

--- common Data Analysis/DMK_readImage.m
+++ common Data Analysis/DMK_readImage.m
@@ -5,7 +5,7 @@ clearvars;
 addpath('..\shared functions folder');
 addpath('..\read and write files');
 %% open image as tiff
-[Filename,Foldername] = uigetfile('D:\Data\Mai\2020-09-02\*.tiff','MultiSelect','on');
+[Filename,Foldername] = uigetfile('\\bazar\acousto-optique\Experimental Data\*.tiff','MultiSelect','on');
 
 if iscell(Filename)==0
    Filename = {Filename};
@@ -16,11 +16,11 @@ Nfiles = length(Filename);
 MyDMK = camera('DMK33GR0134')    ;
 MyDMK.format = 8;
 MyDMK.wavelength = 1064e-9;
-MyDMK.IntegrationTime = 100e-6;
+MyDMK.IntegrationTime = 60e-6;
 
 %% import image
-IM = double( importdata([Foldername,Filename{2}]) );
-BG = double( importdata([Foldername,Filename{1}]) );
+IM = double( importdata([Foldername,Filename{1}]) );
+BG = double( importdata([Foldername,Filename{2}]) );
 
 %% exemple 
 % IM = double( importdata('D:\Data\Mai\2020-03-06\MAIN_J0_10us_14596.tiff') );
@@ -39,6 +39,8 @@ ylabel(cb,'Intensity in counts')
 title('no pump')
 set(findall(gcf,'-property','FontSize'),'FontSize',15)
 %% save as png
+
+%% option code to retreive FWHM of gaussian function:
 
 
 
